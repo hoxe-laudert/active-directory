e3102b466888efb763440d9a4aa6526fe7716ac9
Minh Tran <M.Tran@laudert.de>
2019-02-26 16:33:04 +0700

    change configurations for PHP

--- examples/server/index.php
+++ examples/server/index.php
@@ -4,25 +4,22 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 session_start();
 
-$config = [
+$options = [
     'authentication' => [
         'ad' => [
-            'client_id' => '',
-            'client_secret' => '',
-            'enabled' => '1',
-            'directory' => ''
+            'client_id'     => 'ee3a4759-ed2a-4f14-8013-86336edb73eb',
+            'client_secret' => 'weDMA437=!guhymQNHQ42+*',
+            'enabled'       => '1',
+            'directory'     => '',
+            'return_url'    => 'https://active-directory.local/examples/server/'
         ]
     ]
 ];
 
-$request = new \Zend\Http\PhpEnvironment\Request();
-
-$ad = new \Magium\ActiveDirectory\ActiveDirectory(
-    new \Magium\Configuration\Config\Repository\ArrayConfigurationRepository($config),
-    Zend\Psr7Bridge\Psr7ServerRequest::fromZend(new \Zend\Http\PhpEnvironment\Request())
-);
-
-$entity = $ad->authenticate();
+$request = \Zend\Psr7Bridge\Psr7ServerRequest::fromZend(new \Zend\Http\PhpEnvironment\Request());
+$config = new \Magium\Configuration\Config\Repository\ArrayConfigurationRepository($options);
+$adapter = new \Magium\ActiveDirectory\ActiveDirectory($config, $request);
+$entity = $adapter->authenticate();
 
 echo $entity->getName() . '<Br />';
 echo $entity->getOid() . '<Br />';
