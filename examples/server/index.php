594b957a8b43204355709a182d93f940b8f07101
Minh Tran <M.Tran@laudert.de>
2019-02-26 16:56:28 +0700

    remove app configuration info.

--- examples/server/index.php
+++ examples/server/index.php
@@ -7,19 +7,19 @@ session_start();
 $options = [
     'authentication' => [
         'ad' => [
-            'client_id'     => 'ee3a4759-ed2a-4f14-8013-86336edb73eb',
-            'client_secret' => 'weDMA437=!guhymQNHQ42+*',
+            'client_id'     => '',
+            'client_secret' => '',
             'enabled'       => '1',
             'directory'     => '',
-            'return_url'    => 'https://active-directory.local/examples/server/'
+            'return_url'    => ''
         ]
     ]
 ];
 
 $request = \Zend\Psr7Bridge\Psr7ServerRequest::fromZend(new \Zend\Http\PhpEnvironment\Request());
-$config = new \Magium\Configuration\Config\Repository\ArrayConfigurationRepository($options);
+$config  = new \Magium\Configuration\Config\Repository\ArrayConfigurationRepository($options);
 $adapter = new \Magium\ActiveDirectory\ActiveDirectory($config, $request);
-$entity = $adapter->authenticate();
+$entity  = $adapter->authenticate();
 
 echo $entity->getName() . '<Br />';
 echo $entity->getOid() . '<Br />';
